3ef873931eff832cb354af5c7c0b9a48f333ef18
Artur <aratinskii@gmail.com>
2021-12-24 14:22:29 +0200

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [time,setTime] = useState(0);
+  const [start,setStart] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (start) {
+      interval = setInterval(()=> {
+        setTime(prevTime => prevTime+10)
+      },10)
+    } else {
+      clearInterval(interval);
+    }
+    return() => clearInterval(interval)
+  },[start])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Секундомер</h1>
+    <span>{("0" + (time / 60000) % 60).slice(-2)}</span>
+    <span>{("0" + (time / 1000) % 60).slice(-2)}</span>
+    <span>{("0" + (time / 10) % 1000).slice(-2)}</span>
+    <div>
+      <button onClick={()=> setStart(true)} class="start">Старт</button>
+      <button onClick={()=> setStart(false)}class="stop">Стоп</button>
+      <button onClick={()=> {setTime(0); setStart(false);}} class="reset">Перезапуск</button>
+    </div>
     </div>
   );
 }
