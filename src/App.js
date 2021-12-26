4cb60972b4c2f056208b3b04177ba7d05044f5ac
Artur <aratinskii@gmail.com>
2021-12-26 21:25:22 +0200

    push

--- src/App.js
+++ src/App.js
@@ -1,35 +1,94 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useCallback, useEffect, useState, useMemo } from "react";
+import { Observable, Subject } from "rxjs";
+import { map, buffer, debounceTime, filter, takeUntil } from "rxjs";
+import "./App.css";
 
-function App() {
-  const [time,setTime] = useState(0);
-  const [start,setStart] = useState(false);
+const App = () => {
+  const [state, setState] = useState("stop");
+  const [time, setTime] = useState(0);
+
+  const stop$ = useMemo(() => new Subject(), []);
+  const click$ = useMemo(() => new Subject(), []);
+
+  const start = () => {
+    setState("start");
+  };
+
+  const stop = useCallback(() => {
+    setTime(0);
+    setState("stop");
+  }, []);
+
+  const reset = useCallback(() => {
+    setTime(0);
+  }, []);
+
+  const wait = useCallback(() => {
+    click$.next();
+    setState("wait");
+    click$.next();
+  }, [click$]);
 
   useEffect(() => {
-    let interval = null;
-    if (start) {
-      interval = setInterval(()=> {
-        setTime(prevTime => prevTime+10)
-      },10)
-    } else {
-      clearInterval(interval);
-    }
-    return() => clearInterval(interval)
-  },[start])
+    const doubleClick$ = click$.pipe(
+      buffer(click$.pipe(debounceTime(300))),
+      map((list) => list.length),
+      filter((value) => value >= 2)
+    );
+    const timer$ = new Observable((observer) => {
+      let count = 0;
+      const intervalId = setInterval(() => {
+        observer.next((count += 1));
+      }, 1000);
+
+      return () => {
+        clearInterval(intervalId);
+      };
+    });
+
+    const subscribtion$ = timer$
+      .pipe(takeUntil(doubleClick$))
+      .pipe(takeUntil(stop$))
+      .subscribe({
+        next: () => {
+          if (state === "start") {
+            setTime((prev) => prev + 1);
+          }
+        },
+      });
+
+    return () => {
+      subscribtion$.unsubscribe();
+    };
+  }, [click$, state, stop$]);
+
+  const Time = (Sec) => {
+    const hours = Math.floor(Sec / 3600);
+    const minutes = Math.floor(Sec / 60);
+    const seconds = Sec % 60;
+    return `${hours}:${minutes}:${seconds}`;
+  };
 
   return (
     <div className="App">
-    <h1>Секундомер</h1>
-    <span>{("0" + (time / 60000) % 60).slice(-2)}</span>
-    <span>{("0" + (time / 1000) % 60).slice(-2)}</span>
-    <span>{("0" + (time / 10) % 1000).slice(-2)}</span>
-    <div>
-      <button onClick={()=> setStart(true)} class="start">Старт</button>
-      <button onClick={()=> setStart(false)}class="stop">Стоп</button>
-      <button onClick={()=> {setTime(0); setStart(false);}} class="reset">Перезапуск</button>
-    </div>
+      <div>
+      <h1>Секундомер</h1>
+      <span>{Time(time)}</span>
+      </div>
+      <button className="start" onClick={start}>
+        Start
+      </button>
+      <button className="stop" onClick={stop}>
+        Stop
+      </button>
+      <button className="reset" onClick={reset}>
+        Reset
+      </button>
+      <button className="wait" onClick={wait}>
+        Wait
+      </button>
     </div>
   );
-}
+};
 
 export default App;
